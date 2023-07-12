9c4cb04aa720e8699cededbc148e8e28c6a76c26
SornDev <sorndev.teach@gmail.com>
2023-07-12 21:22:54 +0700

    instal HTML5 Template, created controller & model VDO 03

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ import './bootstrap';
 
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
+import Menu from './components/Menu.vue'
 
 const app = createApp(App)
+app.component('Menubar',Menu);
+app.use(router);
 app.mount('#app-vue');
